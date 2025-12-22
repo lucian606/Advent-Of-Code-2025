c40785b106d62657db22df2af5ee2b23454ceeed
lucian606 <iliesculucian1508@gmail.coom>
2025-12-23 01:44:17 +0200

    Add Day12

---
+++ src/main/kotlin/Day12.kt
@@ -0,0 +1,45 @@
+class Day12(filePath: String) : DaySolver(filePath) {
+    override fun solvePartOne(input: List<String>): String {
+        val sizes = mutableListOf<Int>()
+        var valid = 0
+        var currentSize = 0
+        for (line in input) {
+            if (line.contains("x")) {
+                val parts = line.split(": ")
+                val size = parts[0].split("x").map {it.toInt()}
+                val totalArea = size[0] * size[1]
+                var giftsArea = 0
+                val giftCounts = parts[1].split(" ").map { it.toInt() }
+                for (i in giftCounts.indices) {
+                    giftsArea += (sizes[i] * giftCounts[i])
+                }
+
+                if (giftsArea <= totalArea) {
+                    valid++
+                }
+            } else if (line.contains('#')){
+                for (char in line) {
+                    if (char == '#') {
+                        currentSize++
+                    }
+                }
+            } else if (line.contains(":")) {
+                currentSize = 0
+            } else if (line.isBlank()) {
+                sizes.add(currentSize)
+            }
+        }
+
+        return valid.toString()
+    }
+
+    override fun solvePartTwo(input: List<String>): String {
+        return "ALMOST THERE..."
+    }
+}
+
+fun main() {
+    val path = "src/main/inputs/day12.in"
+    val day = Day12(path)
+    day.printSolution()
+}
